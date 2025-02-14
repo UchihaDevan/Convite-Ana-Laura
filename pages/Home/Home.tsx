5355dc03ad06dc054b5224fc9b07629f6ec69632
error: 404 <45114712+UchihaDevan@users.noreply.github.com>
2025-02-13 23:24:56 -0300

    correção build

--- pages/Home/Home.tsx
+++ pages/Home/Home.tsx
@@ -2,9 +2,6 @@ import Details from '../Details/Details';
 import Welcome from '../Welcome/Welcome';
 import NotFound from '../NotFound/NotFound';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Confirmation from '../../components/Confirmation/Confirmation';
-// import Information from '../../components/Information/Information';
-// import RSVP from '../../components/RSVP/RSVP';
 
 
 function Home() {
